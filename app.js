ab69bf48f96900f5620f9adcbb3fb136501bd2db
Luis Iván García Luna <luis.garcialuna@outlook.com>
2015-12-28 12:36:23 -0600

    Sign up

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const jade = require('jade')
 const fs = require('fs')
 const sendgrid = require('sendgrid')(process.env.SENDG_ID)
-// const html = jade.renderFile('/views/email/email.jade',{name: 'template correo'})
 const app = express()
 const compHtml = jade.renderFile('./views/email/email.jade',{name: 'Template Correo'})
 
@@ -14,12 +13,14 @@ app.use(express.static('public'))
 app.get('/register',function(req,res){
   res.render('register/index')
 })
-
+app.get('/email',function(req,res){
+  res.render('email/email')
+})
 app.post('/register',function(req,res){
   sendgrid.send({
-  to:       'luis.garcialuna@outlook.com',
+  to:       req.body.email,
   from:     'no-reply@testing.com',
-  subject:  'Post Registre',
+  subject:  'Post Registro',
   html:     compHtml
   }, function(err, json) {
   if (err) { return console.error(err); }
